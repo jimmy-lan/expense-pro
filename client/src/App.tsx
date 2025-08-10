5e7c173ff5103c027546a5c0c9e1c15ef588c5d3
Jimmy Lan <jimmylanbcn@gmail.com>
2025-08-09 23:03:45 -0700

    Add route to confirm sign out

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { HomePage, SigninPage, SignupPage, MySpacesPage } from "./pages";
+import {
+  HomePage,
+  SigninPage,
+  SignupPage,
+  MySpacesPage,
+  SignoutConfirmPage,
+} from "./pages";
 
 function App() {
   return (
@@ -10,6 +16,7 @@ function App() {
         <Route path="/login" element={<SigninPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/my" element={<MySpacesPage />} />
+        <Route path="/logout/confirm" element={<SignoutConfirmPage />} />
       </Routes>
     </Router>
   );
